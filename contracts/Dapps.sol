4ac6f9322304d47b12a0d01da124d3c361871f2d
Hayzed-codes <raheemhorlah@gmail.com>
2024-04-27 12:34:27 +0100

    first commit

---
+++ contracts/Dapps.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Dapps {
+    uint storedData = 20;
+    string firstName = "Opa pay";
+    bool isActive = true;
+
+    function set() {
+        // statement block
+    }
+
+    function get() {
+        // statement block
+    }
+
+    function store() {
+
+        uint storage;
+
+        function set(uint x) public{
+            storage = x;
+        }
+
+        function get() public view return(uint){ 
+            return storage
+        }
+
+
+    }
+}
